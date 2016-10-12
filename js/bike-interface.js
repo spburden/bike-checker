456ae8eded86ea8c0c83d146b3e11ffa0e9741b0
Garrett Boggs and Stephen Burden <garrett+stephen@>
2016-10-12 11:47:26 -0700

    add toUppercase with stolen API in Portland, Oregon

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,12 +1,18 @@
 //var apiKey = require('./../.env').apiKey;
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var bikeModel = $("#bike-model").val();
+    $("#bike-info").empty();
+    var bikeModel = toTitleCase($("#bike-model").val());
+    alert(bikeModel);
     var result;
-    
-    $.get('https://bikeindex.org:443/api/v2/bikes_search?page=1&access_token=629e57ce4c332bc4c5a48d5039f21a2bda71b402220fbe07a1b4781990888443')
+
+    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=20000&proximity=45.521728%2C-122.67326&proximity_square=100&access_token=629e57ce4c332bc4c5a48d5039f21a2bda71b402220fbe07a1b4781990888443')
     .then(function(response) {
       console.log(response);
       for (var i = 0; i < response.bikes.length; i++) {
